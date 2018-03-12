d95d08752f61ad1ed2e81ede9d40320e10c04546
liushijiao <liushijiao1@jd.com>
2018-03-12 14:00:45 +0800

    新建项目

---
+++ TestDemo/TestDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TestDemo
+//
+//  Created by 刘世娇 on 2018/3/12.
+//  Copyright © 2018年 刘世娇. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TestDemo/TestDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TestDemo
+//
+//  Created by 刘世娇 on 2018/3/12.
+//  Copyright © 2018年 刘世娇. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
